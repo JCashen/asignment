1d834a7997c92fbfa8c9199639a52d10f03dfe02
JCashen <JCashen@qa.com>
2020-11-16 00:55:24 +0000

    added getenv

--- Game_reviews/application/__init__.py
+++ Game_reviews/application/__init__.py
@@ -1,14 +1,13 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
-
 from os import getenv
 
 app = Flask(__name__)
 
 db = SQLAlchemy(app)
 
-app.config['SQLALCHEMY_DATABASE_URI'] = 'mysql+pymysql://root:Bungle_11@35.242.174.199/game_review'
-app.config['SECRET_KEY'] = 'SECRETKEY'
+app.config['SQLALCHEMY_DATABASE_URI'] = getenv('DATABASE_URI')
+app.config['SECRET_KEY'] = getenv('SECRET_KEY')
 
 from application import routes
 
